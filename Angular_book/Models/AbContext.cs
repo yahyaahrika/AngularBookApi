8ee6e361d04410ece26f49fca60fd21b163d6942
yahya ahrika <zerowapo@gmail.com>
2020-04-23 21:43:44 +0000

    Add project files.

---
+++ Angular_book/Models/AbContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Angular_book.Models
+{
+    public partial class AbContext : DbContext
+    {
+        public AbContext()
+        {
+        }
+
+        public AbContext(DbContextOptions<AbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Book> Book { get; set; }
+
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning 
+//                //To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+//                optionsBuilder.UseSqlServer("server=DESKTOP-HA0PHG4; database=Ab; integrated security=true");
+//            }
+//        }
+
+//        protected override void OnModelCreating(ModelBuilder modelBuilder)
+//        {
+//            modelBuilder.Entity<Book>(entity =>
+//            {
+//                entity.HasNoKey();
+
+//                entity.Property(e => e.Author)
+//                    .HasMaxLength(50)
+//                    .IsUnicode(false);
+
+//                entity.Property(e => e.Id)
+//                    .HasColumnName("id")
+//                    .ValueGeneratedOnAdd();
+
+//                entity.Property(e => e.PublishedAt).HasColumnType("datetime");
+
+//                entity.Property(e => e.Title)
+//                    .HasMaxLength(50)
+//                    .IsUnicode(false);
+//            });
+
+//            OnModelCreatingPartial(modelBuilder);
+//        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Angular_book/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Angular_book.Models
+{
+    public partial class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int? NumberOfPages { get; set; }
+        public DateTime? PublishedAt { get; set; }
+    }
+}
